5bf173fd990a196942825325a47fbaae83a275bd
aslmm <aslmmon993@gmail.com>
2022-06-29 02:43:24 +0200

    initial Commit

---
+++ app/src/main/java/com/example/playingwithcompose/WellnessScreen.kt
@@ -0,0 +1,25 @@
+package com.example.playingwithcompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column {
+        StatefulCounter(modifier)
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )    }
+}
+
