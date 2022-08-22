f9048d49867f73c9f3a38d425ad70606a748d112
Aslm <aslmmon993@gmail.com>
2022-08-22 14:02:00 +0200

    added backgroundShape&& color for Card

--- app/src/main/java/com/example/playingwithcompose/BiznessCardApp/BizCardMainActivity.kt
+++ app/src/main/java/com/example/playingwithcompose/BiznessCardApp/BizCardMainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.playingwithcompose.R
+import com.example.playingwithcompose.WellnessScreen
 import com.example.playingwithcompose.ui.theme.PlayingWithComposeTheme
 
 class BizCardMainActivity : ComponentActivity() {
@@ -43,14 +48,46 @@ fun CreateBizCard() {
         Card(
             modifier = Modifier
                 .width(200.dp)
-                .height(390.dp), elevation = 10.dp,
+                .height(390.dp)
+                .padding(10.dp), elevation = 20.dp,
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            backgroundColor = Color.Black
+            backgroundColor = Color.White
         ) {
 
-        }
-        Text(text = "BizCard Title hello")
+            createImageProfileCard()
 
+        }
+
+    }
+}
+
+@Composable
+fun createImageProfileCard() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.2.dp, Color.LightGray),
+        elevation = 5.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "prof",
+            modifier = Modifier.size(10.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
+@Preview
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PlayingWithComposeTheme {
+        CreateBizCard()
     }
 }
 
