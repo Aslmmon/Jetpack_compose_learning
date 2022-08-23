6c0d7da02f6163fdabc4ebf9f80e4a1560ae054b
Aslm <aslmmon993@gmail.com>
2022-08-24 00:43:37 +0200

    init calculator app

---
+++ app/src/main/java/com/example/playingwithcompose/JetCalculator/JetCalculator.kt
@@ -0,0 +1,45 @@
+package com.example.playingwithcompose.JetCalculator
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.playingwithcompose.ui.theme.PlayingWithComposeTheme
+
+class JetCalculator : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            MyTipCalculatorApp {
+                Text(text = "hello calculator")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MyTipCalculatorApp(content: @Composable () -> Unit) {
+    PlayingWithComposeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    PlayingWithComposeTheme {
+        MyTipCalculatorApp {
+            Text(text = "hello calculator")
+        }
+    }
+}
+
