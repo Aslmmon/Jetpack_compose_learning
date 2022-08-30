6f0c1c9f4706e34776e513c5828dffe46bb76767
Aslm <aslmmon993@gmail.com>
2022-08-30 10:39:30 +0200

    added input Text Field for MainContent.kt

--- app/src/main/java/com/example/playingwithcompose/JetCalculator/JetCalculator.kt
+++ app/src/main/java/com/example/playingwithcompose/JetCalculator/JetCalculator.kt
@@ -24,8 +24,6 @@ class JetCalculator : ComponentActivity() {
         setContent {
             MyTipCalculatorApp {
                 Column {
-
-
                     TopCard()
                     MainContent()
                 }
--- app/src/main/java/com/example/playingwithcompose/JetCalculator/views/MainContent.kt
+++ app/src/main/java/com/example/playingwithcompose/JetCalculator/views/MainContent.kt
@@ -1,24 +1,56 @@
 package com.example.playingwithcompose.JetCalculator.views
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import com.example.playingwithcompose.JetCalculator.views.component.InputTextField
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+     val keyboard = LocalSoftwareKeyboardController.current
     Surface(
-        modifier = Modifier.padding(10.dp).fillMaxWidth().fillMaxHeight(),
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
         shape = RoundedCornerShape(corner = CornerSize(5.dp)),
         border = BorderStroke(2.dp, color = Color.LightGray),
     ) {
+        Column {
 
+            InputTextField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    keyboard?.hide()
+                }
+            )
+
+        }
     }
 }
